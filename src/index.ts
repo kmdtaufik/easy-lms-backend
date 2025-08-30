c658307218335bec1f89b8018d3185b2c63933d3
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-08-30 13:48:11 +0600

    fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -56,6 +56,7 @@ app.get("/", arcjetMiddleware(), (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`✅ Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`✅ Server running on port ${port}`);
+// });
+export default app;
